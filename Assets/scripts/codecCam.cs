617cff682d5140776aee99e30bde4a8ba301491d
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-05-13 22:11:55 +0100

    Camera turns off when robot isnt active still TODO::
    change main camera when clicking on a side camera
    
    there is a strange bug where the robot does not move to the position when placed bu i do not know why this bug occurs

---
+++ Assets/scripts/codecCam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class codecCam : MonoBehaviour
+{
+    public Renderer[] codecCams;
+    public PlayerControlNoJump[] robots;
+    public Material[] robotCam;
+    public Material offScreen;
+
+    private void Update()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (robots[i].IsDisabled())
+            {
+                codecCams[i].material = offScreen;
+            }
+            else
+            {
+                codecCams[i].material = robotCam[i];
+            }
+        }
+    }
+}
