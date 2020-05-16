32348678dc5d5b90b034f4899664e3187aa93906
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-05-16 22:48:59 +0100

    added new concept for deploying bots

---
+++ Assets/scripts/Player/Orb.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+    public float halfHeight = .3f;
+    private Rigidbody rb;
+    private PlayerControlNoJump[] robots = new PlayerControlNoJump[4];
+    int layerMask = 1 << 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        layerMask = ~layerMask;
+    }
+
+    public void SetBots(ref PlayerControlNoJump[] bots)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            robots[i] = bots[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGrounded();
+    }
+
+    void CheckGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, halfHeight))
+        {
+            Debug.DrawRay(transform.position+Vector3.up*0.1f, -Vector3.up *hit.distance, Color.yellow, layerMask);
+            foreach (PlayerControlNoJump script in robots)
+            {
+                if (!script.IsDisabled())
+                {
+                    continue;
+                }
+                //ideally its the Y rot of the orb and not quat.identity
+                script.EnableBot(transform.position, Quaternion.identity);
+                break;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/Player/PlayerAltControl.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAltControl : Player
+{
+    public Transform orbSpawnPoint;
+    public GameObject Orb;
+    public PlayerControlNoJump[] robot;
+    private bool activePlayer = true;
+
+    void Start()
+    {
+        PlayerStart();
+    }
+    void Update()
+    {
+        StateSwitch();
+        
+        //below two MUST be before Jump() for jumping to work
+        CalculateGround();
+            
+        Gravity();
+        
+        if (activePlayer)
+        {
+            GetInput();
+
+            CalculateCamera();
+
+            MovePlane();
+
+            Jump();
+
+            BuildBot();
+
+            BreakBot();
+        }
+        else
+        {
+            velocity = new Vector3(0, velocity.y, 0);
+        }
+        
+        jones.Move(velocity * Time.deltaTime);
+    }
+
+    void StateSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            activePlayer = !activePlayer;
+        }
+    }
+
+    /*
+     * If you can build, build a robot to that location
+     */
+    void BuildBot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Build Button press");
+            GameObject obj = Instantiate(Orb, orbSpawnPoint.position, orbSpawnPoint.rotation);
+            obj.GetComponent<Orb>().SetBots(ref robot);
+        }
+    }
+
+    /*
+     * destroy a robot you are looking at
+     */
+    void BreakBot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            // Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 1 << 8;
+
+            //invert layer bitmask
+            layerMask = ~layerMask;
+
+            RaycastHit hit;
+            // Does the ray intersect any objects except the player
+            if (Physics.Raycast(camera.transform.position, camera.forward, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(camera.transform.position, camera.forward * hit.distance, Color.red);
+                if (hit.transform.CompareTag("robot"))
+                {
+                    hit.transform.gameObject.GetComponent<PlayerControlNoJump>().DisableBot();
+                }
+            }
+        }
+    }
+}
