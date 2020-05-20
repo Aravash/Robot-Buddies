f4211ec0fbe97d9b70e24347ab253f7a182ab715
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-05-20 23:16:14 +0100

    basic game manager
    working on lighting

---
+++ Assets/scripts/AudioDirector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDirector : MonoBehaviour
+{
+    public Animator[] mechArmAnim = new Animator[2];
+    public AudioClip[] voiceLines;
+
+    private void Start()
+    {
+        StartCoroutine(Intro());
+    }
+
+    private IEnumerator Intro()
+    {
+        Debug.Log("Track 1 Plays");
+        yield return new WaitForSeconds(2);
+        mechArmAnim[0].enabled = true;
+        yield return new WaitForSeconds(2);
+        mechArmAnim[1].enabled = true;
+    }
+}
